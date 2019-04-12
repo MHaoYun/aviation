24bde68be2235e8db5f7557db34b290488a0cc38
Eric Yan <digimonyan@gmail.com>
2019-04-12 19:40:58 +0800

    airline, aircraft and wiki-disaster

---
+++ Data/py2neo/database.py
@@ -0,0 +1,31 @@
+from py2neo import Graph, Node, Relationship, NodeMatcher
+import csv
+
+
+def database1():
+    my_graph = Graph("http://localhost:7474", username="neo4j", password="123456")
+
+    csvFile = open("airline_data.csv", "r")
+    reader = csv.reader(csvFile)
+    for item in reader:
+        print(item)
+
+        if item[0] != "Airline":
+            node_airline = Node(
+                "Airline", name=item[0], ICAO=item[1], IATA=item[2], Callsign=item[3]
+            )
+            if item[5] != "":
+                node_airline.update(website=item[5])
+            my_graph.create(node_airline)
+
+            matcher = NodeMatcher(my_graph)
+            tar = matcher.match("Country", name=item[4]).first()
+            if tar:
+                airline_locates_in = Relationship(node_airline, "locate", tar)
+                my_graph.create(airline_locates_in)
+            else:
+                node_country = Node("Country", name=item[4])
+                my_graph.create(node_country)
+                airline_locates_in = Relationship(node_airline, "locate", node_country)
+                my_graph.create(airline_locates_in)
+
---
+++ Data/py2neo/database2.py
@@ -0,0 +1,49 @@
+from py2neo import Graph, Node, Relationship, NodeMatcher
+import csv
+
+
+def database2():
+    my_graph = Graph("http://localhost:7474", username="neo4j", password="123456")
+
+    csvFile = open("my_python/plane_data_2.csv", "r")
+    reader = csv.reader(csvFile)
+
+    i = 0
+    for item in reader:
+        i = i + 1
+        print(i)
+        if item[0] != "Aircraft Type":
+            node_plane = Node(
+                "Plane",
+                Type=item[0],
+                Register_num=item[2],
+                Delivered_date=item[4],
+                Status=item[5],
+            )
+            my_graph.create(node_plane)
+
+            matcher = NodeMatcher(my_graph)
+            tar = matcher.match("Manufactor", name=item[1]).first()
+            if tar:
+                plane_manufactured_by = Relationship(node_plane, "Manufactured_by", tar)
+                my_graph.create(plane_manufactured_by)
+            else:
+                node_manufactor = Node("Manufactor", name=item[1])
+                my_graph.create(node_manufactor)
+                plane_manufactured_by = Relationship(
+                    node_plane, "Manufactured_by", node_manufactor
+                )
+                my_graph.create(plane_manufactured_by)
+
+            tar2 = matcher.match("Airline", name=item[3]).first()
+            if tar2:
+                plane_belong_to = Relationship(node_plane, "Belong_to", tar2)
+                my_graph.create(plane_belong_to)
+            else:
+                node_airline = Node("Airline", name=item[3])
+                my_graph.create(node_airline)
+                plane_belong_to = Relationship(node_plane, "Belong_to", node_airline)
+                my_graph.create(plane_belong_to)
+
+    print("finished")
+
